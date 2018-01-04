8faefb5aea88308fc1931fdf1540165013d1a8bf
Petter <petter@maksimer.no>
2018-01-04 21:07:50 +0100

    Welcome Home

---
+++ home.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: petter
+ * Date: 04/01/2018
+ * Time: 21:06
+ */
+
+class Home
+{
+    public function welcomeHome() {
+        echo 'Welcome Home';
+    }
+}
